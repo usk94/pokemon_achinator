8e8eb0b1d307ff582154737309d8fb1e25f917e0
Yusaku <hitoyasablue@gmail.com>
2024-12-16 22:52:39 +0900

    uddate main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+pub mod pokemon;
+use pokemon::Pokemon;
+use serde_json::from_str;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://pokeapi.co/api/v2/pokemon/1";
+    let response = reqwest::get(url).await?.text().await?;
+
+    let pokemon: Pokemon = from_str(&response)?;
+    println!("{:#?}", pokemon);
+    Ok(())
 }
